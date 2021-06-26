56b8149fd05bbbb7301c0fc8c97687722f89c690
Jeanette Yue <yue.jeanette@gmail.com>
2021-06-25 21:42:57 -0700

    Create CamelliaTheme obj, which handles event delegation and theme state

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,30 @@
 // Put your application javascript here
+import { delegator } from "./events.js";
+
+console.log("application.js");
+
+const CamelliaTheme = (() => {
+  console.log(delegator);
+
+  const state = {
+    collection: {
+      tags: {},
+      products: {},
+    },
+    quickview: {},
+    product: {},
+    cart: {},
+    user: {},
+  };
+
+  return {
+    updateState: (key, change) => {
+      state[key] = change;
+      console.log(state);
+    },
+    updateSubState: (parentKey, key, change) => {
+      state[parentKey][key] = change;
+      console.log(state);
+    },
+  };
+})();
